dbce1bb20f5ae3199876fc4d4e63b6f18257089d
Lucas Tiberio <ext.lucas.tiberio@farfetch.com>
2022-09-27 11:10:06 -0300

    feat: Added axios retry interceptor

--- src/integration/axiosInstance.ts
+++ src/integration/axiosInstance.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import axiosRetry from 'axios-retry';
 import { API_HOST } from '../common/consts';
 
 const axiosClient = axios.create();
@@ -16,5 +17,17 @@ axios.interceptors.request.use(request => {
 
 //All request will wait 2 seconds before timeout
 axiosClient.defaults.timeout = 2000;
+
+
+axiosRetry(axios, {
+  retries: 2,
+  retryDelay: (retryCount) => {
+    return retryCount * 2000; // interval between retries
+  },
+  retryCondition: (error) => {
+    // if retry condition is not specified, by default idempotent requests are retried
+    return error.response?.status === 503;
+  },
+});
 
 export default axiosClient;
